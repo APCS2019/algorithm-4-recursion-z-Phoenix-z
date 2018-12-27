98b20a8b5b44009c6f704811a9cc37bd0b65d41b
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2018-12-27 23:14:47 +0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,67 +1,37 @@
 public class Recursion{
    //part1: factorial non-recursion
-    public class recursion {
-    public static void rec(int n) {
-
-        if (n == 1) {
-            return;
+   public static void rec(int n){ 
+     int a = 1;
+        for (int i = 1; i < n+1; i++) {
+            a *= i;
         }
-        rec(n - 1);
-        System.out.println(n);
-    }
-
-    public static int factorial(int n) {
-        if (n == 1 || n == 0) {
-            return 1;
-        }
-        return n * factorial((n - 1));
-    }
-
-    public static void main(String[] args) {
-        int n = 4;
-        System.out.println(factorial(n));
-    }
-}
-   
+        return a;
+   }
    //part2: factorial recursion
-      public class recursion {
-    public static void rec(int n) {
-
-        if (n == 1) {
-            return;
-        }
-        rec(n - 1);
-        System.out.println(n);
-    }
-
     public static int factorial(int n) {
         if (n == 1 || n == 0) {
             return 1;
         }
         return n * factorial((n - 1));
     }
-
-    public static void main(String[] args) {
-        int n = 4;
-        System.out.println(factorial(n));
-    }
-}
-   
-   
    //part3: fibonacci sequence non-recursion
-   
-   
-   
+   public static int fibonacci1(int n) {
+        int first = 1;         
+        int second = 1;
+        for (int i = 1; i < n; i++) {
+             if (i > 2) {
+
+                int temp = first;
+               first = second;
+                second += temp;
+            }
+        }
+        return second + first;
+    }
    //part4: fibonacci sequence recursion
-  public class Fibo {
     public static int fibonacci(int n) {
         if (n == 1 || n == 0) { 
             return 1; 
         } 
         return fibonacci(n - 1) + fibonacci(n - 2); 
     } 
-    public static void main(String[] args) {
-        System.out.println(fibonacci((20)) / (double) fibonacci(21));
-    }
-}
-
